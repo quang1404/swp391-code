499d6e6aa27babb852bfc37545dd0d767550f42a
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-09 21:32:00 +0700

    update(demo deploy)

--- koicare/ecosystem.config.js
+++ koicare/ecosystem.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    apps : [{
-      name: "your-app-name", 
-      script: "./app.js", 
-      instances: 1, 
-      exec_mode: "cluster", 
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      }
-    }]
-  };
+  apps : [{
+    name: "koicare", 
+    script: "./app.js", 
+    instances: 1, 
+    exec_mode: "cluster", 
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {  
+      NODE_ENV: "production",
+      DATABASE_HOST: "localhost", 
+    }
+  }]
+};
