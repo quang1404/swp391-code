153231674da5fd96a4b48f57333db35357188991
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-14 10:42:27 +0700

    add input validation for role

--- koicare/models/role.js
+++ koicare/models/role.js
@@ -17,14 +17,18 @@ const getRoleById = (id, callback) => {
 
 // Update role by ID
 const updateRoleById = (id, updatedRoleData, callback) => {
+ 
+    if (!updatedRoleData.name) { 
+      return callback(new Error('Invalid input data. Role name is required.'), null);
+    }
     const query = `UPDATE Role SET ? WHERE id = ?`;
     db.query(query, [updatedRoleData, id], (error, results) => {
-        if (error) {
-            return callback(error, null);
-        }
-        return callback(null, results.affectedRows);
+      if (error) {
+        return callback(error, null);
+      }
+      return callback(null, results.affectedRows);
     });
-};
+  };
 
 module.exports = {
     getRoleById,
