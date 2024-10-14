a8103a8f0c1fcdacb98b89e8877b77f487ec0493
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-14 10:52:31 +0700

    update waterValue

--- koicare/models/waterValue.js
+++ koicare/models/waterValue.js
@@ -2,52 +2,60 @@ const db = require('../config/db');
 
 // Get water param value by id of water and name of value
 const getWaterParamByIdAndName = (id, name, callback) => {
-    const query = `SELECT param_value FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`; // Removed single quotes around ?
+    const query = `SELECT param_value FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`;
     db.query(query, [id, name], (error, results) => {
         if (error) {
             return callback(error, null);
         }
         if (results.length > 0) {
-            return callback(null, results[0]); 
+            return callback(null, results[0]);
         } else {
-            return callback(null,   
- null); 
+            return callback(null,
+                null);
         }
     });
 };
 
 // Create water param value
 const createWaterParam = (name, param_value, water_parameters_id, callback) => {
+    if (!name || !param_value || !water_parameters_id) {
+        return callback(new Error('Invalid input data. Please check all fields.'), null);
+    }
+
     const query = `
-        INSERT INTO Water_parameter_value (name, param_value, water_parameters_id)  
-        VALUES (?, ?, ?);`; // Corrected table name
+    INSERT INTO Water_parameter_value (name, param_value, water_parameters_id)  
+    VALUES (?, ?, ?);`;
     db.query(query, [name, param_value, water_parameters_id], (error, results) => {
         if (error) {
             return callback(error, null);
         }
-        return callback(null, results.affectedRows); 
+        return callback(null, results.affectedRows);
     });
 };
 
 // Update water param value by id of water and name of value
 const updateWaterParamByIdAndName = (id, name, updateValue, callback) => {
-    const query = `UPDATE Water_parameter_value SET param_value = ? WHERE water_parameters_id = ? AND name = ?;`; // Removed single quotes around ?
+    if (!updateValue) {
+        return callback(new Error('Invalid input data. Param value is required.'), null);
+    }
+
+    const query = `UPDATE Water_parameter_value SET param_value = ? WHERE water_parameters_id = ? AND name = ?;`;
     db.query(query, [updateValue, id, name], (error, results) => {
         if (error) {
             return callback(error, null);
         }
-        return callback(null, results.affectedRows); 
+        return callback(null, results.affectedRows);
     });
 };
 
 // Delete water param by id of water and name of value
 const deleteWaterParam = (id, name, callback) => {
-    const query = `DELETE FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`; // Removed single quotes around ?, added FROM
+    const query = `DELETE FROM Water_parameter_value WHERE water_parameters_id = ? AND name = ?;`;
     db.query(query, [id, name], (error, results) => {
         if (error) {
             return callback(error, null);
         }
-        return callback(null, results.affectedRows); 
+        return callback(null, results.affectedRows);
     });
 };
 
@@ -58,14 +66,14 @@ const getAllWaterParams = (callback) => {
         if (error) {
             return callback(error, null);
         }
-        return callback(null, results); 
+        return callback(null, results);
     });
 };
 
 module.exports = {
     getWaterParamByIdAndName,
-    createWaterParam, 
+    createWaterParam,
     updateWaterParamByIdAndName,
     deleteWaterParam,
-    getAllWaterParams 
+    getAllWaterParams
 };
