b4de54626c0b062db4bc94e14e2ba6b2855add70
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-14 11:18:56 +0700

    update ecosystem

--- koicare/ecosystem.config.js
+++ koicare/ecosystem.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  apps : [{
-    name: "koicare", 
-    script: "./app.js", 
-    instances: 1, 
-    exec_mode: "cluster", 
+  apps: [{
+    name: "koicare",
+    script: "./app.js",
+    instances: 1,
+    exec_mode: "cluster",
     env: {
       NODE_ENV: "development",
     },
-    env_production: {  
+    env_production: {
       NODE_ENV: "production",
-      DATABASE_HOST: "localhost", 
+      DATABASE_HOST:localhost,
+      DATABASE_USER:root,
+      DATABASE_PASSWORD:123456789,
+      DATABASE :koicare,
+      JWT_SECRET: "very_secret_password"
     }
   }]
 };
