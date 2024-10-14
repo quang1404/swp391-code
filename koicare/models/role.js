cbb5b5d6a5b2a587374aa499d30d4029567fb47d
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-14 10:50:53 +0700

    update role&user

--- koicare/models/role.js
+++ koicare/models/role.js
@@ -1,36 +1,78 @@
 const db = require('../config/db');
 
+// Get all roles
+const getAllRoles = (callback) => {
+  const query = `SELECT * FROM Role;`;
+  db.query(query, (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results);
+  });
+};
+
 // Get role by ID
 const getRoleById = (id, callback) => {
-    const query = `SELECT * FROM Role WHERE id = ?`;
-    db.query(query, [id], (error, results) => {
-        if (error) {
-            return callback(error, null);
-        }
-        if (results.length > 0) {
-            return callback(null, results[0]); 
-        } else {
-            return callback(null, null); 
-        }
-    });
+  const query = `SELECT * FROM Role WHERE id = ?`;
+  db.query(query, [id], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (results.length > 0) {
+      return callback(null, results[0]);
+    } else {
+      return callback(null, null);
+    }
+  });
 };
+
+// Create a new role
+const createRole = (name, callback) => {
+  if (!name) {
+    return callback(new Error('Role name is required.'), null);
+  }
+
+  const query = `INSERT INTO Role (name) VALUES (?)`;
+  db.query(query, [name], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results.affectedRows);
+  });
+};
+
 
 // Update role by ID
 const updateRoleById = (id, updatedRoleData, callback) => {
- 
-    if (!updatedRoleData.name) { 
-      return callback(new Error('Invalid input data. Role name is required.'), null);
+
+  if (!updatedRoleData.name) {
+    return callback(new Error('Invalid input data. Role name is required.'), null);
+  }
+
+  const query = `UPDATE Role SET ? WHERE id = ?`;
+  db.query(query, [updatedRoleData, id], (error, results) => {
+    if (error) {
+      return callback(error, null);
     }
-    const query = `UPDATE Role SET ? WHERE id = ?`;
-    db.query(query, [updatedRoleData, id], (error, results) => {
-      if (error) {
-        return callback(error, null);
-      }
-      return callback(null, results.affectedRows);
-    });
-  };
+    return callback(null, results.affectedRows);
+  });
+};
+
+// Delete role by ID
+const deleteRoleById = (id, callback) => {
+  const query = `DELETE FROM Role WHERE id = ?`;
+  db.query(query, [id], (error, results) => {
+    if (error) {
+      return callback(error, null);
+    }
+    return callback(null, results.affectedRows);
+  });
+};
 
 module.exports = {
-    getRoleById,
-    updateRoleById
+  getAllRoles,
+  getRoleById,
+  createRole,
+  updateRoleById,
+  deleteRoleById
 };
--- koicare/models/user.js
+++ koicare/models/user.js
@@ -1,4 +1,20 @@
-const db = require('../config/db'); 
+const db = require('../config/db');
+
+// Create user 
+const createUser = (userData, callback) => {
+    const { name, email, password, password_confirm, role_id } = userData;
+    if (!name || !email || !password || password !== password_confirm) {
+        return callback(new Error('Invalid input data'), null);
+    }
+
+    const query = `INSERT INTO users (name, email, password, role_id) VALUES (?, ?, ?, ?)`;
+    db.query(query, [name, email, password, role_id], (error, results) => {
+        if (error) {
+            return callback(error, null);
+        }
+        return callback(null, results.affectedRows);
+    });
+};
 
 // Get user by ID
 const getUserById = (id, callback) => {
@@ -8,25 +24,52 @@ const getUserById = (id, callback) => {
             return callback(error, null);
         }
         if (results.length > 0) {
-            return callback(null, results[0]); 
+            return callback(null, results[0]);
         } else {
-            return callback(null, null); 
+            return callback(null, null);
         }
     });
 };
 
-// Update user by ID
+// Get user by email (used for login)
+const getUserByEmail = (email, callback) => {
+    const query = `SELECT * FROM users WHERE email = ?`;
+    db.query(query, [email], (error, results) => {
+        if (error) {
+            return callback(error,null);
+        }
+        if (results.length > 0) {
+            return callback(null, results[0]);
+        } else {
+            return callback(null,null);
+        }
+    });
+};
+
 const updateUserById = (id, updatedUserData, callback) => {
+    if (updatedUserData.name && updatedUserData.name.length === 0) {
+      return callback(new Error('Name cannot be empty'), null);
+    }
+    if (updatedUserData.email && !isValidEmail(updatedUserData.email)) {
+      return callback(new Error('Invalid email format'), null);
+    }
     const query = `UPDATE users SET ? WHERE id = ?`;
     db.query(query, [updatedUserData, id], (error, results) => {
-        if (error) {
-            return callback(error, null);
-        }
-        return callback(null, results.affectedRows); 
+      if (error) {
+        return callback(error, null);
+      }
+      return callback(null, results.affectedRows);
     });
-};
+  };
 
-module.exports = {
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  module.exports = {
     getUserById,
+    getUserByEmail, 
+    createUser, 
     updateUserById
-};
+  };
