35b23ceaf830eff2e241d5f9821bb2ddc0a41912
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-19 00:08:56 +0700

    update db.js

--- koicare/config/db.js
+++ koicare/config/db.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config({ path: '../.env' });
 
 const databaseConfig = {
-  host: process.env.HOST || "127.0.0.1",
-  port: process.env.PORT || 3333,
+  host: process.env.DATABASE_HOST || "127.0.0.1",
+  port: process.env.DATABSE_PORT || 3333,
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "123456789",
   database: process.env.DATABASE || "koicaredb",
