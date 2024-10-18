355fe3e2e74ccbc9a2f30ba73d1382bbcbb4e3a4
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-18 23:49:15 +0700

    update code to deploy

--- koicare/routes/homeRoutes.js
+++ koicare/routes/homeRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.send("hello world")
+    // res.render('index');
 });
 
 router.get('/register', (req, res) => {
