461d378800c091bae5a0f0430406f99058330165
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-19 00:02:58 +0700

    update db.js

--- koicare/config/db.js
+++ koicare/config/db.js
@@ -2,13 +2,15 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config({ path: '../.env' });
 
-const db = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3333,
-  user: "root",
-  password: "123456789",
-  database: "koicaredb",
-});
+const databaseConfig = {
+  host: process.env.HOST || "127.0.0.1",
+  port: process.env.PORT || 3333,
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "123456789",
+  database: process.env.DATABASE || "koicaredb",
+}
+
+const db = mysql.createConnection();
 
 // db.connect((error) => {
 //   if (error) {
