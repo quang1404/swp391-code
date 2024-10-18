2899977b5f931abd2e428e33989fb8fd91f84466
Ken <102181792+KietNguyen279@users.noreply.github.com>
2024-10-19 00:57:43 +0700

    fix env db

--- koicare/config/db.js
+++ koicare/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const databaseConfig = {
   host: process.env.DATABASE_HOST || "127.0.0.1",
-  port: process.env.DATABSE_PORT || 3333,
+  port: process.env.DATABASE_PORT || 3333,
   user: process.env.DATABASE_USER || "root",
   password: process.env.DATABASE_PASSWORD || "123456789",
   database: process.env.DATABASE_NAME || "koicaredb"
